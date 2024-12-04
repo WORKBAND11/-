268686159c2eea944f86e0f4dfb82d4f0d344f4c
WORKBAND11 <komarik.n.v.2011@gmail.com>
2024-12-04 11:18:33 +0700

    Add files via upload

---
+++ Второе задание, сложный уровень.py
@@ -0,0 +1,13 @@
+number = int(input("Введите число от 1 до 10:" )) #Вводим число, которое надо будет посчитать
+if number <= 0 or number > 10: #Вводим ограничения по вводимым числам (можно только от 1 до 10)
+    print("Число не то!")
+print(number, "* 1 =", number * 1)
+print(number, "* 2 =", number * 2)
+print(number, "* 3 =", number * 3)
+print(number, "* 4 =", number * 4)
+print(number, "* 5 =", number * 5)
+print(number, "* 6 =", number * 6)
+print(number, "* 7 =", number * 7)
+print(number, "* 8 =", number * 8)
+print(number, "* 9 =", number * 9)
+print(number, "* 10 =", number * 10) #И очень запутанным способом (избейте меня) выводим каждое вычисление))
